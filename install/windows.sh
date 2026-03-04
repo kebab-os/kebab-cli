150007538e25445c5357e176ddafda48a9ef291d
7ames <123058@combertonvc.org>
2026-03-04 10:20:09 +0000

    Update windows.sh

--- install/windows.sh
+++ install/windows.sh
@@ -3,4 +3,4 @@ if (!(Test-Path .kebab)) { mkdir .kebab }
 cd .kebab
 curl.exe -L https://github.com/kebab-os/kebab-cli/archive/refs/heads/main.zip -o kebab-cli.zip
 Expand-Archive -Path kebab-cli.zip -DestinationPath . -Force
-cd kebab-cli
+cd kebab-cli/src
